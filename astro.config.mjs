9855a8726cda87626b21a8f75a57ec334f6b548d
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2025-09-06 01:18:25 +0200

    refactor: migrate to astro

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  build: {
+    format: "preserve",
+  },
+});
